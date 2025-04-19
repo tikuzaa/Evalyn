9bbeca996af99a171ea12b48be04c485ca0fb747
Bidisha <bidishaabanik@gmail.com>
2025-04-19 21:55:36 +0530

    interviewcard feedback updated

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -5,9 +5,10 @@ import { getRandomInterviewCover } from '@/lib/utils';
 import Link from 'next/link';
 import { Button } from './ui/button'; 
 import DisplayTechIcons from './DisplayTechIcons';
+import { getFeedbackByInterviewId } from '@/lib/actions/general.action';
 
-const InterviewCard = ( { interviewId, userId, role, type, techstack, createdAt}: InterviewCardProps ) => {
-    const feedback = null as Feedback | null;
+const InterviewCard = async ( { interviewId, userId, role, type, techstack, createdAt}: InterviewCardProps ) => {
+    const feedback = userId && interviewId ? await getFeedbackByInterviewId({interviewId, userId}) : null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed':type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
   return (
